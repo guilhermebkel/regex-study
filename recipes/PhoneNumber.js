641eed48608910ac6795192a2195db4e4de51d85
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-09-30 18:22:51 -0300

    docs(recipe): add phone number recipe

---
+++ recipes/PhoneNumber.js
@@ -0,0 +1,11 @@
+const text = `
+Lista telefônica:
+	- (21) 12345-6789
+	- (11) 62300-2234
+	- 5678-7771
+	- (85)3333-7890
+	- (1) 4321-1234
+`
+
+const phoneNumber = /(\(\d{2}\)\s?)?\d{4,5}-\d{4}/g
+console.log(text.match(phoneNumber))
